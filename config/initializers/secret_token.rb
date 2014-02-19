1a9a21d987dfef1e9f32610cc594d6606d93238d
John Otander <johnotander@gmail.com>
2014-02-19 13:46:34 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sundial::Application.config.secret_key_base = '0aeb7060bbce5c31debd070986528f7ae876670efdf5e2d5bb705f32cf4de04095ec53b6509445b18c4c97bdf33e87d11e9d3346189c70c0a5c385fc069fd2c9'
