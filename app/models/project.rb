446f5ee6a2334343244081852453cf4b7eeb19b6
John Otander <johnotander@gmail.com>
2014-02-19 19:29:00 -0700

    Make some specs pass with some basic model relationships.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+  belongs_to :user
+  has_many :tasks
+  has_many :blocks, through: :tasks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :projects
+  has_many :tasks, through: :projects
+  has_many :blocks, through: :tasks
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, email_format: true
 end
